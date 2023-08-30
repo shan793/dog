629b5cc4f0fda61c9d003dcd064e448b445192ce
shan793 <sgoonatilaka09@gmail.com>
2023-08-30 15:43:22 -0400

    Removed Readme and added some functions

---
+++ dog.go
@@ -0,0 +1,11 @@
+package dog
+
+
+func Mama() string {
+	return "Mama dog protects!"
+}
+
+func Bark() string {
+	return "Woofs *in defiance*!"
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shan793/dog
+
+go 1.21.0
